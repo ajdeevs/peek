b155aa8adbb9a1aa4d90f9a86be0d28d0ef005dc
Ankit Bhawnani <bhawnaniankit@gmail.com>
2025-10-26 19:28:00 +0000

    version:1.0.1 - bug fix

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import fs from "fs"
-import process from 'process';
+import fs from "fs";
 import path from "path";
-import {colorizeFile} from "./color.js";
+import { colorizeFile } from "./color.js";
 
 const program = new Command();
 
-program.
-name("peek")
-.description("A modern logging cli tool writtern in js")
-.action(()=>{
-    const logPath = path.join(process.cwd(),"./");
-    const files=fs.readdirSync(logPath);
-    files.forEach(file=>{
-        if(!file.startsWith(".")) 
-            process.stdout.write(colorizeFile(file)+" ");
-    })
-    console.log();
-})
-
 program
-.option("-a","List all files and directories including hidden")
-.action(()=>{
-    const logPath = path.join(process.cwd(),"./");
-    const files=fs.readdirSync(logPath);
-    files.forEach(file=>{
-        process.stdout.write(colorizeFile(file)+" ");
-    })
-    console.log();
-})
+  .name("peek")
+  .description("A modern logging CLI tool written in JS")
+  .option("-a, --all", "List all files including hidden")
+  .action((options) => {
+    const logPath = process.cwd();
+    const files = fs.readdirSync(logPath);
 
-program.parse();
+    files.forEach((file) => {
+      if (!options.all && file.startsWith(".")) return;
+      process.stdout.write(colorizeFile(file) + " ");
+    });
+
+    console.log();
+  });
+
+program.parse(process.argv);
